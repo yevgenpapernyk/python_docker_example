7e298d95bf2c8b3a542f9499ac5b4f88010bb2ff
Yevgen Papernyk <44113339+yevgenpapernyk@users.noreply.github.com>
2023-04-10 14:34:09 +0000

    Add more info to html in both app.py

--- 1_flask/app.py
+++ 1_flask/app.py
@@ -1,8 +1,18 @@
+import os
+from datetime import datetime
+
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<html><body><h1>Hello, Docker!</h1></body></html>'
+    return f'<html>' \
+           f'<head><title>Hello Docker</title></head>' \
+           f'<body>' \
+           f'<h1>Hello Docker!</h1>' \
+           f'<p>Date: {datetime.now()}<p>' \
+           f'<p>Load: {os.getloadavg()}<p>' \
+           f'</body>' \
+           f'</html>'
 
--- 2_flask_and_mongodb/app.py
+++ 2_flask_and_mongodb/app.py
@@ -4,19 +4,28 @@ from flask import Flask
 from pymongo import MongoClient
 
 
-MONGO_URI = os.getenv('MONGO_URI')
-
+# app
 app = Flask(__name__)
+
+# env
+MONGO_URI = os.getenv('MONGO_URI')  # default: None -> then 'mongodb://localhost:27017' is used
+MONGO_DB_NAME = os.getenv('MONGO_DB_NAME', 'pugn')
+MONGO_COLLECTION_NAME = os.getenv('MONGO_COLLECTION_NAME', 'test')
+
+# mongo
 client = MongoClient(MONGO_URI)
-db = client['pugn']
-collection = db['test']
+db = client[MONGO_DB_NAME]
+collection = db[MONGO_COLLECTION_NAME]
 
 
 @app.route('/')
 def hello_world():
     count = collection.count_documents({})
-    return f'<html><body>' \
+    return f'<html>' \
+           f'<head><title>Hello Docker</title></head>' \
+           f'<body>' \
            f'<h1>Hello, Docker!</h1>' \
-           f'<p>We have {count} documents in the collection.</p>' \
-           f'</body></html>'
+           f'<p>We have {count} documents in the collection "{collection.name}".</p>' \
+           f'</body>' \
+           f'</html>'
 
